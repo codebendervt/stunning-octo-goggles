d6c0a594c157031e4d05f245c0540eef47be1e7f
codebender <rawk@sauveur.xyz>
2020-12-22 20:32:39 +0200

    updated the fetch command

--- technical/core/fetch/fetch.js
+++ technical/core/fetch/fetch.js
@@ -1,44 +1,50 @@
 
 
-const getData = async (url, method = "GET", isAPI = false, data = null,key=null) => {
-  let headers = { 'Content-Type': 'application/json' }
-  if(key){
-    headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${key}`
+const getData = async (url, method = "GET", isAPI = false, data = null,key=null,options=null) => {
+
+  if(options == null){
+    let headers = { 'Content-Type': 'application/json' }
+    if(key){
+      headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${key}`
+      }
     }
-  }
-
-  let options = {
-    method:method,
-    headers: headers
-  }
-
-  if (method == "POST") {
-
-    let body = JSON.stringify(data)
-    options = {
-      method:method,
-      headers : headers,
-      body: body
-    }
-  }
-
-
-  if (isAPI) {
   
-    headers = {
-      'Content-Type': 'application/json',
-      "Host": "api.sauveur.cloud",
-      "Ocp-Apim-Subscription-Key": "b319671d160a48e6b7efc1db27591fda",
-      "Ocp-Apim-Trace": true
-    }
-
-    options = {
-      method: method,
+    let options = {
+      method:method,
       headers: headers
     }
+  
+    if (method == "POST") {
+  
+      let body = JSON.stringify(data)
+      options = {
+        method:method,
+        headers : headers,
+        body: body
+      }
+    }
+  
+  
+    if (isAPI) {
+    
+      headers = {
+        'Content-Type': 'application/json',
+        "Host": "api.sauveur.cloud",
+        "Ocp-Apim-Subscription-Key": "b319671d160a48e6b7efc1db27591fda",
+        "Ocp-Apim-Trace": true
+      }
+  
+      options = {
+        method: method,
+        headers: headers
+      }
+    }
   }
+
+
+ 
 
   // Default options are marked with *
   const response = await fetch(url, options
@@ -53,7 +59,14 @@ const getData = async (url, method = "GET", isAPI = false, data = null,key=null)
   //   //body: JSON.stringify(data) // body data type must match "Content-Type" header
   // }
   );
-  return response.json()
+  if(options ==null){
+    return response.json()
+  }else{
+    console.log(response)
+    return response.statusText
+  }
+
+
 
 };
 
