29089e591fab66089f0a2be2e79f02d0db77b9d8
codebender <rawk@sauveur.xyz>
2020-12-30 14:16:51 +0200

    now it is just routine

--- styles/index.js
+++ styles/index.js
@@ -7,7 +7,7 @@ export default {
     input : "appearance-none w-full px-2 placeholder-grey bg-transparent text-2xl m-2 font-default-regular",
     input_border : "border-l-2 border-white input-border max-w-md flex items-center",
     title : "font-default-title text-5xl",
-    form_button : "p-8 text-2xl font-default-accent disabled:opacity-50 disabled:text-grey",
+    form_button : "p-8 text-2xl font-default-accent disabled:opacity-50 disabled:text-grey appearance-none",
     col: col,
     col_center: `${col} ${center}`,
     row: row,
