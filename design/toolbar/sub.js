3c2ad66b5e063ecabaa82546f334e04abbe2265d
codebender <lionelmbankeu@gmail.com>
2021-01-19 15:48:56 +0200

    updated the nav for sub toolbar

--- design/toolbar/sub.js
+++ design/toolbar/sub.js
@@ -1,23 +1,41 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-export default function SubToolbar({list}) {
+export default function SubToolbar({list,landing}) {
+    const router = useRouter();
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
 
         document.addEventListener('swiped-right', function (e) {
-            console.log(e.target); // the element that was swiped
-            console.log(e.detail.dir); // swiped direction
+            // setCount(count + 1)
+            // handleClick(list[count])
 
+        });
+
+        document.addEventListener('swiped-left', function (e) {
+            // console.log(e.target); // the element that was swiped
+            // console.log(e.detail.dir); // swiped direction
+            // handleClick(list[count])
+            // setCount(count - 1)
         });
         return () => {
             console.log('Clean up')
         }
     }, [])
 
-    const SmallCard = () => {
+    const handleClick = (active) => {
+
+        router.push(`${landing}/${active}?active=${active}`)
+      }
+
+
+    const SmallCard = ({name}) => {
         return (
-            <div className=" w-1/3 lg:w-36 h-16 p-2">
-                <div className="w-full h-full rounded shadow-md lg:shadow hover:shadow-none bg-gray-800 hover:bg-gray-900"></div>
+            <div onClick={() => handleClick(name)} className=" w-1/3 lg:w-36 h-16 p-2">
+                <div className="w-full h-full rounded shadow-md lg:shadow hover:shadow-none bg-gray-800 hover:bg-gray-900 text-center items-center flex justify-center text-xl font-default-title cursor-emoji capitalize ">
+                  {name}
+                </div>
 
             </div>
         )
@@ -28,9 +46,7 @@ export default function SubToolbar({list}) {
 
         <div className="w-full h-32 flex flex-row py-3 lg:p-4">
             {
-                list.map((i)=> {
-                    <SmallCard />
-                })
+                list.map((i,k) => <SmallCard key={k} name={i}  />)
             }
            
         </div>
