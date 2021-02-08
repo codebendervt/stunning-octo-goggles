02c484118299cfdd8c13e1003b2efbe3886bb92f
codebender <lionelmbankeu@gmail.com>
2021-02-08 20:48:21 +0200

    updated soon convert to legacy

--- technical/elements/components/index.js
+++ technical/elements/components/index.js
@@ -1,14 +1,14 @@
-import Button from './button'
-import Label from './typography'
-import Form from './form'
-import TextList from './list/list.text/'
-import Card from './card/'
-import {SmCard,LgCard} from './card/cards/'
-import {InputText,InputNumber} from './input/inputs/'
-import Select from './input/select/'
-import Alert from './alerts/'
-import {Splash} from './loader'
+// import Button from './button'
+// import Label from './typography'
+// import Form from './form'
+// import TextList from './list/list.text/'
+// import Card from './card/'
+// import {SmCard,LgCard} from './card/cards/'
+// import {InputText,InputNumber} from './input/inputs/'
+// import Select from './input/select/'
+// import Alert from './alerts/'
+// import {Splash} from './loader'
 
-export {Button, Label, TextList,Card,SmCard,LgCard,InputText,InputNumber,Form,Select,Alert,Splash}
+// export {Button, Label, TextList,Card,SmCard,LgCard,InputText,InputNumber,Form,Select,Alert,Splash}
 
-//these components are for commerce products
+// //these components are for commerce products
--- technical/elements/index.js
+++ technical/elements/index.js
@@ -1,4 +1,4 @@
-import {Card, Label,SmCard,LgCard,InputText,InputNumber,Button,Form,Select,Alert,Splash} from './components'
+// import {Card, Label,SmCard,LgCard,InputText,InputNumber,Button,Form,Select,Alert,Splash} from './components'
 
 
-// export {Card,Label,SmCard,LgCard,InputText,InputNumber,Button,Form,Select,Alert,Splash};
+// // export {Card,Label,SmCard,LgCard,InputText,InputNumber,Button,Form,Select,Alert,Splash};
