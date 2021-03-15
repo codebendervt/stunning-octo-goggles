6addc1ff20434850a4cf568807f182aff585ba09
codebender <47385943+rawkthethinker@users.noreply.github.com>
2021-03-15 18:00:45 +0200

    added new fonts

--- technical/core/browser/index.js
+++ technical/core/browser/index.js
@@ -1,45 +1,45 @@
-import url from './url'
+// import url from './url'
 
 
-var Firefox = "Firefox"
+// var Firefox = "Firefox"
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    checkBrowser();
-});
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+//     checkBrowser();
+// });
 
 
-const hello = () =>{
-    console.log('hello from the old guard ❤️')
-}
+// const hello = () =>{
+//     console.log('hello from the old guard ❤️')
+// }
 
-function checkBrowser(){
-    if (navigator.userAgent.indexOf(Firefox) !== -1) {
-        console.log('congratutlations on being safe')
-        let version  =  parseInt(process.env.REACT_APP_BROWSER);
-        let current = parseInt(navigator.userAgent.split('/')[3]);
+// function checkBrowser(){
+//     if (navigator.userAgent.indexOf(Firefox) !== -1) {
+//         console.log('congratutlations on being safe')
+//         let version  =  parseInt(process.env.REACT_APP_BROWSER);
+//         let current = parseInt(navigator.userAgent.split('/')[3]);
        
-        console.log(current >= version )
+//         console.log(current >= version )
 
-    } else {
-        console.log('why you not being safe')
-    }
-}
+//     } else {
+//         console.log('why you not being safe')
+//     }
+// }
 
-switch (document.readyState) {
-    case "loading":
-        console.log('I am still loading')
-        break;
-    case "interactive":
-        // The document has finished loading. We can now access the DOM elements.
-        // But sub-resources such as images, stylesheets and frames are still loading.
-       console.log('let us be intereactive')
-        break;
-    case "complete":
-        // The page is fully loaded.
-       checkBrowser()
-        break;
-}
+// switch (document.readyState) {
+//     case "loading":
+//         console.log('I am still loading')
+//         break;
+//     case "interactive":
+//         // The document has finished loading. We can now access the DOM elements.
+//         // But sub-resources such as images, stylesheets and frames are still loading.
+//        console.log('let us be intereactive')
+//         break;
+//     case "complete":
+//         // The page is fully loaded.
+//        checkBrowser()
+//         break;
+// }
 
 
-export {url,hello};
+// export {url,hello};
